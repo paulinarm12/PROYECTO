61feabdb890ee04fbe74afb8a2fe6e2c3c1fbfba
pauled12 <rodriguezmpau2@gmail.com>
2025-06-28 19:27:16 -0600

    cALENDARIO DOCNETES

--- ProyectoADS/mi-proyecto/components/ui/calendar.tsx
+++ ProyectoADS/mi-proyecto/components/ui/calendar.tsx
@@ -1,6 +1,16 @@
-// components/ui/calendar.tsx
-"use client"
 import * as React from "react"
-export function Calendar() {
-  return <div>Calendario</div>
+import { DayPicker } from "react-day-picker"
+import "react-day-picker/dist/style.css"
+
+export type CalendarProps = React.ComponentProps<typeof DayPicker>
+
+export function Calendar({ className, ...props }: CalendarProps) {
+  return (
+    <DayPicker
+      className={className}
+      showOutsideDays
+      fixedWeeks
+      {...props}
+    />
+  )
 }
